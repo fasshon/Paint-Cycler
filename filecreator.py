69627ba45023ccb2037d020ff86db3007a0ab115
fashion <153262187+fasshon@users.noreply.github.com>
2025-07-03 23:04:28 -0600

    Create filecreator.py

---
+++ filecreator.py
@@ -0,0 +1,7 @@
+open("badgelist.txt", "w")
+open("namelist.txt", "w")
+open("Userlist.txt", "w")
+open("viewlist.txt", "w")
+open("colorlist.txt", "w")
+with open("config.txt", "w") as f:
+    f.write("on\non\non")
